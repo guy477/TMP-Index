58ad60d81e21ec87eab3b2dcb09855dbd1a8b964
Guy477 <tpratt021@gmail.com>
2019-10-22 14:40:59 -0500

    Updated Tweets.csv

--- src/sent.py
+++ src/sent.py
@@ -2,21 +2,33 @@ import sys
 import re
 import csv
 import json
+import string
 
-cred = json.load(open('../data/condensed_2018.json'))
 #print(cred)
 
-tweets = open('../data/tweets.csv', 'w', encoding='utf-8')
 
-# create the csv writer object
+# get tweet data and create the csv writer object
 
+
+"""
+cred = json.load(open('../data/condensed_2018.json'))
+tweets = open('../data/tweets.csv', 'w', encoding='utf-8', newline="")
 csvwrite = csv.writer(tweets)
 
 
 for i in cred:
     tweet = i['text'].lower()
-    tweet = re.sub(r"[,.]", "", tweet)
-    #print(tweet)
+    # ’ and “ are special characters.
+    tweet = re.sub(r"["+string.punctuation+"“’]*", "", tweet)
+    #|
+    tweet = re.sub(r"http(\w)*", "", tweet)
+    if "\"" in tweet:
+        print(tweet)
     csvwrite.writerow([tweet])
 tweets.close()
-    
+
+"""
+
+#ToDo:
+#   Train a sentiment analysis model on trumps tweets
+
